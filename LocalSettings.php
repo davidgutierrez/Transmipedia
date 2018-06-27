fd3f3cf8b78b57ed224e5baad3813ae418626e14
David Gutierrez <1551324+davidgutierrez@users.noreply.github.com>
2018-06-26 23:59:23 -0500

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -77,3 +77,5 @@ $wgScribuntoDefaultEngine = 'luastandalone';
 $wgShowExceptionDetails = true;
 $wgShowDBErrorBacktrace = true;
 $wgShowSQLErrors = true;
+
+$wgEnableUploads = true; # Enable uploads
